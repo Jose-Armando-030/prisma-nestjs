2c3cbb3f040f17cfda17720a3dd3a5d00c962da1
Jose Armando <samojose030@gmail.com>
2024-02-09 20:46:32 -0400

    documentacion README.md

---
+++ src/task/task.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Task } from '@prisma/client';
+
+@Injectable()
+export class TaskService {
+  // Vamos a definir metodos que se van a comunicar con la base de datos
+
+  constructor(private prisma: PrismaService) {}
+
+  async getAllTask(): Promise<Task[]> {
+    return this.prisma.task.findMany();
+  }
+
+  async getTaskById(id: number): Promise<Task> {
+    return this.prisma.task.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async createTask(data: Task): Promise<Task> {
+    return this.prisma.task.create({
+      data,
+    });
+  }
+
+  async updateTask (id: number, data: Task): Promise<Task> {
+    return this.prisma.task.update({
+      where: {
+        id
+      },
+      data
+    })
+  }
+
+  async deleteTask (id: number): Promise<Task> {
+    return this.prisma.task.delete({
+      where: {
+        id
+      }
+    })
+  }
+
+}
